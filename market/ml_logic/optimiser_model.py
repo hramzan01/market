07e6307988c683ef1616f514e7efef1246b09166
Adam Oxley <adam.oxley@hotmail.co.uk>
2024-03-18 15:28:44 +0000

    function outline set up for final optimsation model

---
+++ market/ml_logic/optimiser_model.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from scipy.optimize import minimize
+from scipy.optimize import Bounds
+import matplotlib.pyplot as plt
+
+
+def optimiser_model():
+    '''
+    A model which takes in the results of three seperate models:
+    Energy consumption, PV Energy Gen, Energy Price
+    and outputs a prediction based on when to buy and sell
+    along with the total profitability of the period
+    '''
+    return
+
+
+def baseline_model():
+    '''
+    A model which takes in the results of three seperate models:
+    Energy consumption, PV Energy Gen, Energy Price
+    and outputs a baseline profitability
+    '''
+    return
