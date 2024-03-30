d34da13a081e633730e642bd922cd1f8de00d3f9
rahul iyer <iyer.rrahul@gmail.com>
2024-03-30 09:03:44 +0000

    API with weather code

--- market/api/fast_price.py
+++ market/api/fast_price.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from energy_price_pred.energypricepred import *
-
-from market.ml_logic.optimiser_model_variable_inputs_copy_ri import *
+import json
+from market.ml_logic.optimiser_model_variable_inputs_copy_ri2 import *
 
 import matplotlib.pylab as plt
 
@@ -19,12 +19,20 @@ def predict(battery_size: int, # 5 total size
     output_keys = ['SalePrice_p/kwh', 'PurchasePrice_p/kwh', 'Generation_kwh', 'Consumption_kwh']
 
     res = run_full_model_api(int(battery_size), int(battery_charge))
+
+    # res_dict = json.loads(res)
+
     res_pred_saleprice = pd.DataFrame.from_dict(res['predicted_data']['SalePrice_p/kwh']) #pd.DataFrame.from_dict(res['predicted_data'])
-    res_pred_all= pd.DataFrame.from_dict(res['predicted_data'])
+    res_pred_all= res['predicted_data']#pd.DataFrame.from_dict(res['predicted_data'])
     res_opt_batt= pd.DataFrame(res['optimised_battery_storage'])
     res_opt_buyprice= pd.DataFrame(res['optimised_energy_purchase_price'])
     res_opt_sellprice= pd.DataFrame(res['optimised_energy_sold_price'])
     res_opt_baseprice= pd.DataFrame(res['baseline_hourly_price'])
+    res_weather_code = res['weather_code']
+    res_weather_code_reset = res_weather_code.reset_index()
+    res_weather_code_reset = res_weather_code_reset.drop(columns=['ds'])
+    res_weather_code_reset = res_weather_code_reset['weather_code'].tolist()
+    # res_weather_code_reset = pd.DataFrame(res_weather_code_reset)
     key_list=[]
     value_list=[]
     data_saleprice = res_pred_saleprice['SalePrice_p/kwh']
@@ -32,8 +40,10 @@ def predict(battery_size: int, # 5 total size
         key_list.append(key)
         value_list.append(value)
     #return {'res':f'key is {key_list[0]} of type {type(key_list[0])} and value is {value_list[0]}'} #{"res columns": res_pred_saleprice}
-    output = {'prediction_data': res_pred_all, 'res_opt_batt': res_opt_batt, 'res_opt_buyprice': res_opt_buyprice, 'res_opt_sellprice': res_opt_sellprice, 'res_opt_baseprice': res_opt_baseprice}
-    return output
+    output = {'prediction_data': res_pred_all, 'res_opt_batt': res_opt_batt, 'res_opt_buyprice': res_opt_buyprice,
+              'res_opt_sellprice': res_opt_sellprice, 'res_opt_baseprice': res_opt_baseprice,
+              'res_weather_code': res_weather_code_reset}
+    return output# {'keys': str(res.keys())}
 
 @app.get("/")
 def root():
